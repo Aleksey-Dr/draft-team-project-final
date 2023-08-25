f244ed690c1616f2d98fe0ac8eb756388da34c76
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2023-08-25 19:09:02 +0000

    Deploying to gh-pages from @ Aleksey-Dr/draft-team-project-final@a5bc52c98e33a291f32b048994b523964ed2c855 🚀

---
+++ static/js/285.f5f9b5a2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdraft_team_project_final=self.webpackChunkdraft_team_project_final||[]).push([[285],{285:function(t,e,n){n.r(e),n.d(e,{default:function(){return a}});var r=n(329);function a(){return(0,r.jsx)(r.Fragment,{})}}}]);
+//# sourceMappingURL=285.f5f9b5a2.chunk.js.map
---
+++ static/js/38.2b1a8f1e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdraft_team_project_final=self.webpackChunkdraft_team_project_final||[]).push([[38],{38:function(t,e,n){n.r(e),n.d(e,{default:function(){return a}});var r=n(329);function a(){return(0,r.jsx)(r.Fragment,{})}}}]);
+//# sourceMappingURL=38.2b1a8f1e.chunk.js.map
